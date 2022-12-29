e7d440df9be06186c0096a84f2060faca28a32d3
Panupan Sriautharawong <lionpeal@gmail.com>
2022-12-29 16:42:34 +0700

    Fix mobile navigation

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,11 +2,13 @@
  import Profile from './assets/user.png'
  import Messages from './assets/Messages.png'
  import Home from './assets/home.png'
+ import Logout from './assets/logout.png'
  import { useLogoutUserMutation } from "../services/appApi"
  import { useSelector } from "react-redux";
- import {Link} from "react-router-dom"
+ import {Link, useNavigate } from "react-router-dom"
 
 export default function Navbar() {
+  const navigate = useNavigate();
   const user = useSelector((state) => state.user);
   const [logoutUser] = useLogoutUserMutation();
   async function handleLogout(e) {
@@ -15,24 +17,22 @@ export default function Navbar() {
       // redirect to home page
       window.location.replace("/login");
   }
-  
+
   return (
     <>
       <div className='SideNav'>
     <div className='Options'>
-  
-    <div className='navOption'><img className='icon2' src={Home}/> <a className="route" href='/'>Home</a></div>
-    <div className='navOption'><img  className='icon2' src={Profile}/><a className="route" href={`/user/`}>Profile</a></div>
-    <div className='navOption'><img  className='icon2' src={Messages}/><a className="route"  href='/messages'>Messages</a></div>
-    <div  onClick={handleLogout} className='navOption'><img  className='icon2' src={Messages}/><a className="route"  href='/messages'>Logout</a></div>
+
+    <div onClick={()=> { navigate('/') }} className='navOption'><img className='icon2' src={Home}/><span class="route">Home</span></div>
+    <div onClick={()=> { navigate('/user') }} className='navOption'><img  className='icon2' src={Profile}/><span class="route">Profile</span></div>
+    <div onClick={()=> { navigate('/messages') }} className='navOption'><img  className='icon2' src={Messages}/><span class="route">Messages</span></div>
+    <div onClick={handleLogout} className='navOption'><img  className='icon2' src={Logout}/><span class="route">Logout</span></div>
     </div>
 
   </div>
 
-   
-      
+
+
     </>
   );
 }
- 
-  
